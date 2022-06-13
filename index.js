cd08d6062eaacb6d9972183ed8e684b207122cd5
Jordan Halfman <jordanhalfman@gmail.com>
2022-06-13 11:44:29 -0700

    moved width property to css

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ fetch("https://smashbros-unofficial-api.vercel.app/api/v1/ultimate/characters", 
     const newChar = document.createElement("div");
     const charDiv = document.querySelector("#character-div");
     newChar.innerHTML = `
-    <img src="${character.images.icon}" width="50">
+    <img src="${character.images.icon}">
     `
+    newChar.addEventListener("click", e => {
+      const mainChar = document.createElement("div");
+      const mainCharDiv = document.querySelector("#character-selected");
+      mainChar.innerHTML = `
+      <img src="${character.images.icon}">
+      `
+      mainCharDiv.appendChild(mainChar);
+    })
     charDiv.appendChild(newChar);
 }))
